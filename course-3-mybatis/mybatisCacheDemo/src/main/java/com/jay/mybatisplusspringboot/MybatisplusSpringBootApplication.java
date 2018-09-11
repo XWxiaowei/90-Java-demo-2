076e0d379d81224b45cdf67df96afe5ca2212c72
XWxiaowei <2809641033@qq.com>
2018-09-12 00:02:21 +0800

    mybatisplus-spring-boot

---
+++ course-3-mybatis/mybatisCacheDemo/src/main/java/com/jay/mybatisplusspringboot/MybatisplusSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.jay.mybatisplusspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisplusSpringBootApplication.class, args);
+	}
+}
---
+++ course-3-mybatis/mybatisCacheDemo/src/test/java/com/jay/mybatisplusspringboot/MybatisplusSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jay.mybatisplusspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisplusSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
