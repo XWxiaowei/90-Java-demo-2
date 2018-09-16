36b44847026d5d238ee0608081f11669f98551c4
XWxiaowei <2809641033@qq.com>
2018-09-17 07:02:05 +0800

    代码提交

---
+++ course-6-redis/spring-redis-demo/src/main/java/com/jay/springredisdemo/SpringRedisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jay.springredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisDemoApplication.class, args);
+	}
+}
